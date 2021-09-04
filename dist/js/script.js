bb048925a3f4ad46802c1d1dab010ee682558aee
mananex <2008harutyun@mail.ru>
2021-09-04 18:53:38 +0400

    skills, works and price-list

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,6 +1,6 @@
 const hamburger = document.querySelector('.hamburger'),
       menu      = document.querySelector('.menu'),
-      closeElem     = document.querySelector('.menu__close');
+      closeElem = document.querySelector('.menu__close');
 
 hamburger.addEventListener('click', () => {
     menu.classList.add('active');
@@ -8,4 +8,11 @@ hamburger.addEventListener('click', () => {
 
 closeElem.addEventListener('click', () => {
     menu.classList.remove('active');
+});
+
+const percents = document.querySelectorAll('.item__progress__percent'),
+      values   = document.querySelectorAll('.item__progress__value');
+
+percents.forEach( (item, i) => {
+    values[i].style.width = item.innerHTML;
 });
